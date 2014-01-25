5e50083c1cf1e3256ea431118cc396bd5b1bb984
frank <wumuch@gmail.com>
2014-01-25 20:16:42 +0800

    revision

--- ccpp/meta.hpp
+++ ccpp/meta.hpp
@@ -53,7 +53,7 @@
 
 #ifdef __cplusplus
 #	if defined(__GNUC__)	\
-		&& defined(__GXX_EXPERIMENTAL_CXX0X__) && __GXX_EXPERIMENTAL_CXX0X__
+		&& (!defined(__GXX_EXPERIMENTAL_CXX0X__) || !__GXX_EXPERIMENTAL_CXX0X__)
 #		include <cstdint>
 #	else
 #		include <boost/cstdint.hpp>
